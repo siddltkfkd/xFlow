bda3ab0869c226f842ad1195600dd79c08c146f7
halo-eunsu <ujd017@naver.com>
2023-10-12 14:43:00 +0900

    Wire edit

--- src/main/java/com/nhnacademy/node/Message.java
+++ src/main/java/com/nhnacademy/node/Message.java
@@ -1,5 +1,29 @@
 package com.nhnacademy.node;
 
 public class Message {
+
+    int id;
+    String name;
+    String message;
     
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
 }
--- src/main/java/com/nhnacademy/node/Wire.java
+++ src/main/java/com/nhnacademy/node/Wire.java
@@ -1,15 +1,33 @@
 package com.nhnacademy.node;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
-public class Wire {
+public class Wire{
     
 
     static int count;
     String id;
 
-    Queue<Message> queue;
-    
+    Queue<Message> messageQueue;
+
+    public Wire(){
+        messageQueue = new LinkedList<>();
+    }
+
+    public void put(Message message){
+        messageQueue.add(message);
+    }
+
+    public Message get(){
+        return messageQueue.poll();
+    }
+
+
+
+
+
+
 
 
     
