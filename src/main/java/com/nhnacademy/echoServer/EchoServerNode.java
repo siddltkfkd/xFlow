9d6346a2b8eb91c6b7c52be5ef2adbd88b1c9bc3
halo-eunsu <ujd017@naver.com>
2023-10-12 16:44:50 +0900

    1

--- src/main/java/com/nhnacademy/echoServer/EchoServerNode.java
+++ src/main/java/com/nhnacademy/echoServer/EchoServerNode.java
@@ -1,9 +1,14 @@
 package com.nhnacademy.echoServer;
 
+import java.util.Scanner;
+
 public class EchoServerNode {
     
 
     //terminal에서 String Url 입력
+
+    Scanner sc = new Scanner(System.in);
+    
     
     
     //Socket으로 입력신호 받음
