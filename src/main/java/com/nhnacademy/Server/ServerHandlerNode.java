f99b5b0b5fc586a4f24984359458f9abaabacf53
halo-eunsu <ujd017@naver.com>
2023-10-13 09:50:32 +0900

    edit

--- src/main/java/com/nhnacademy/Server/ServerHandlerNode.java
+++ src/main/java/com/nhnacademy/Server/ServerHandlerNode.java
@@ -1,9 +1,13 @@
 package com.nhnacademy.Server;
 
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import com.nhnacademy.node.InputOutputNode;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 public class ServerHandlerNode extends InputOutputNode {
     public ServerHandlerNode(int inCount, int outCount) {
         super(inCount, outCount);
@@ -13,21 +17,29 @@ public class ServerHandlerNode extends InputOutputNode {
     int port = 12345;
 
     @Override
-    public void process() {
-        super.process();
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            while (!Thread.currentThread().isInterrupted()) {
-                Socket socket = serverSocket.accept();
+    public void run() {
+        preprocess();
+        process();
+        postprocess();
+    }
 
-                
+    @Override
+    public void process() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+                Socket socket = serverSocket.accept();
                 ServerNode echo = new ServerNode(socket);
-                echo.start();
-            }
-        } catch (Exception e) {
-            // TODO: handle exception
+                echo.start();   
+        } catch (IOException e) {
+            log.info("Thread exception");
         }
     }
 
 
+    public static void main(String[] args) {
+        
+        ServerHandlerNode handlerNode = new ServerHandlerNode(2,2);
+
+        handlerNode.start();
+    }
 
 }
--- src/main/java/com/nhnacademy/Server/ServerNode.java
+++ src/main/java/com/nhnacademy/Server/ServerNode.java
@@ -1,41 +1,82 @@
 package com.nhnacademy.Server;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.List;
+import com.nhnacademy.node.InputOutputNode;
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
-public class ServerNode {
-    
+public class ServerNode extends InputOutputNode {
+
     Socket clientSocket;
     List<String> blackList = new LinkedList<>();
+    String clientAddress = "";
+    
 
-    public ServerNode(Socket clientsocket){
+    public ServerNode(Socket clientsocket) {
+        super(2, 2);
         this.clientSocket = clientsocket;
+        clientAddress = clientSocket.getInetAddress().getHostAddress();
     }
 
-    String clientAddress = clientSocket.getInetAddress().getHostAddress();
     
-    public void block(Socket socket){
+    @Override
+    public void preprocess() {
+        super.preprocess();
+        block(clientSocket);
+    }
+
+    public void block(Socket socket) {
+        
         log.info("클라이언트의 ip 주소는 : {} 입니다.", clientAddress);
-        for (String p : blackList){
-            if( p == clientAddress){
+        for (String p : blackList) {
+            if (p == clientAddress) {
                 log.info("해당 주소는 접근이 제한되었습니다.");
                 log.info("제한된 주소 : {}", clientAddress);
-                
+
                 try {
-                    socket.close();    
+                    socket.close();
+                    log.info("접속 종료");
                 } catch (Exception e) {
                     // TODO: handle exception
                 }
-                
             }
         }
-        
+    }
+
+    public void process() {
+        blackList.add("local");
+
+        try (Socket socket = new Socket(clientAddress, clientSocket.getLocalPort())) {
+
+           BufferedReader terminalIn = new BufferedReader(new InputStreamReader(System.in));
+           BufferedWriter terminalOut = new BufferedWriter(new OutputStreamWriter(System.out));
+            BufferedReader socketIn =
+                    new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            BufferedWriter socketOut =
+                    new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+
+            while (!Thread.currentThread().isInterrupted()) {
+                String line = socketIn.readLine() + "\n";
+                socketOut.write(line + "\n");
+                socketOut.flush();
+            }
+
+        } catch (Exception e) {
+            log.info("message exception");
+            // TODO: handle exception
+        }
+
+
+
     }
 
 
-    
 }
