b398dfc5219e87bd6052914685ece4383e83dfe1
insub2004 <insub2004@naver.com>
2023-10-13 17:21:43 +0900

    새로 pull하기 전 커밋

--- src/main/java/com/nhnacademy/exception/OutOfBoundsException.java
+++
@@ -1,5 +0,0 @@
-package com.nhnacademy.exception;
-
-public class OutOfBoundsException extends RuntimeException {
-
-}
--- src/main/java/com/nhnacademy/exception/wireIsFullException.java
+++
@@ -1,5 +0,0 @@
-package com.nhnacademy.exception;
-
-public class wireIsFullException extends RuntimeException {
-
-}
