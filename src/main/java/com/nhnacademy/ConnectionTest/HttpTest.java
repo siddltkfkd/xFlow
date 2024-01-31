355f08f5868ff3e83955297b303662abf339665d
halo-eunsu <ujd017@naver.com>
2024-01-31 16:29:46 +0900

    commit

--- src/main/java/com/nhnacademy/ConnectionTest/HttpTest.java
+++ src/main/java/com/nhnacademy/ConnectionTest/HttpTest.java
@@ -7,11 +7,15 @@ public class HttpTest {
     public static void main(String[] args) {
         
 
+
+
         //
         //
 
         try (ServerSocket serverSocket = new ServerSocket(3000)) {
-            
+
+
+
             
         } catch (Exception e) {
             // TODO: handle exception
